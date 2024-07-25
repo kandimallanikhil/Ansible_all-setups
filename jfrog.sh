a83dbab7dbca8243096c463054ee24c2c2f8c2c5
kandimallanikhil <52294077+kandimallanikhil@users.noreply.github.com>
2024-07-25 12:04:24 +0530

    Update jfrog.sh

--- jfrog.sh
+++ jfrog.sh
@@ -5,3 +5,18 @@ yum update -y
 yum install jfrog-artifactory-oss -y
 systemctl start artifactory.service
 systemctl status artifactory.service
+
+
+===========================================
+cd /opt
+wget https://releases.jfrog.io/artifactory/bintray-artifactory/org/artifactory/oss/jfrog-artifactory-oss/7.68.19/jfrog-artifactory-oss-7.68.19-linux.tar.gz
+tar -zxvf jfrog-artifactory-oss-7.68.19-linux.tar.gz
+cd artifactory-oss-7.68.19/
+cd app/bin/
+./artifactory.sh start
+./artifactory.sh status
+
+
+
+
+
